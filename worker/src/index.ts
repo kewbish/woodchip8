e3f572c8ccf75f0c512247727e2602b37d3877a7
Emilie Ma <kewbish@gmail.com>
2023-05-05 09:34:14 -0700

    Switch to websocket-based updates

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -9,8 +9,19 @@ export default {
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    const id = env.WOODCHIP.idFromName("woodchip-main");
-    const dObj = env.WOODCHIP.get(id);
-    return dObj.fetch(request);
+    if (new URL(request.url).pathname == "/newWoodchip") {
+      const id = env.WOODCHIP.newUniqueId();
+      return new Response(id.toString(), {
+        headers: { "Access-Control-Allow-Origin": "*" },
+      });
+    } else {
+      const reqCopy = request.clone();
+      const json = (await request.json()) as any;
+      if (!json || !json["name"]) {
+        return new Response("No Woodchip room name provided", { status: 400 });
+      }
+      const dObj = env.WOODCHIP.get(json["name"]);
+      return dObj.fetch(request);
+    }
   },
 };
--- worker/src/woodchip.ts
+++ worker/src/woodchip.ts
@@ -1,5 +1,7 @@
 import { Stack } from "stack-typescript";
 
+const HEADERS = { headers: { "Access-Control-Allow-Origin": "*" } };
+
 export class Woodchip implements DurableObject {
   memory: Int8Array = new Int8Array(4096);
   pc: number = 0x200;
@@ -11,15 +13,41 @@ export class Woodchip implements DurableObject {
   shouldQuit: boolean = false;
 
   state: DurableObjectState;
+  sessions: WebSocket[];
 
   constructor(state: DurableObjectState, env: any) {
     this.state = state;
+    this.sessions = [];
   }
 
   async fetch(request: Request) {
     const url = new URL(request.url);
+    const newReq = request.clone();
     const json = await request.json();
 
+    if (url.pathname == "/websocket") {
+      if (request.headers.get("Upgrade") != "websocket") {
+        return new Response("Expected websocket upgrade request", {
+          status: 400,
+        });
+      }
+      const pair = new WebSocketPair();
+      await this.handleSession(pair[1], newReq, url, json);
+      return new Response(null, { status: 101, webSocket: pair[0] });
+    } else {
+      return new Response(
+        "Woodchip uses websockets, try initializing websocket request",
+        { status: 404 }
+      );
+    }
+  }
+
+  async handleSession(
+    websocket: WebSocket,
+    request: Request,
+    url: URL,
+    json: any
+  ) {
     let memory: Int8Array =
       (await this.state.storage.get("memory")) || new Int8Array(4096);
     let pc: number = (await this.state.storage.get("pc")) || 0x200;
@@ -32,85 +60,141 @@ export class Woodchip implements DurableObject {
     let shouldQuit: boolean =
       (await this.state.storage.get("shouldQuit")) || false;
 
-    if (request.method == "POST") {
-      switch (url.pathname) {
-        case "/setMemory":
-          await this.state.storage.put(
-            "memory",
-            (json as { memory: Int8Array })["memory"]
-          );
-          break;
-        case "/setPC":
-          await this.state.storage.put("pc", (json as { pc: number })["pc"]);
-          break;
-        case "/setIndex":
-          await this.state.storage.put(
-            "index",
-            (json as { index: number })["index"]
-          );
-          break;
-        case "/resetRegs":
-          await this.state.storage.put(
-            "regs",
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-          );
-          break;
-        case "/setReg":
-          let newRegs = regs;
-          newRegs[(json as { regIndex: number; value: number }).regIndex] = (
-            json as { regIndex: number; value: number }
-          ).value;
-          await this.state.storage.put("regs", newRegs);
-          break;
-        case "/setStack":
-          await this.state.storage.put(
-            "stack",
-            (json as { stack: number[] })["stack"]
-          );
-          break;
-        case "/setDelayTimer":
-          await this.state.storage.put(
-            "delayTimer",
-            (json as { delayTimer: number })["delayTimer"]
-          );
-          break;
-        case "/setSoundTimer":
-          await this.state.storage.put(
-            "soundTimer",
-            (json as { soundTimer: number })["soundTimer"]
-          );
-          break;
-        case "/setShouldQuit":
-          await this.state.storage.put(
-            "shouldQuit",
-            (json as { shouldQuit: boolean })["shouldQuit"]
-          );
-          break;
-        default:
-          return new Response("Unknown path", { status: 404 });
+    let ready = false;
+    websocket.accept();
+    this.sessions.push(websocket);
+    websocket.addEventListener("message", async (msg: any) => {
+      try {
+        const data = JSON.parse(msg.data);
+        if (!ready) {
+          websocket.send(JSON.stringify({ ready: true }));
+          ready = true;
+        }
+
+        switch (data.commandPath) {
+          case "/setMemory":
+            await this.state.storage.put(
+              "memory",
+              (json as { memory: Int8Array })["memory"]
+            );
+            this.broadcast(
+              JSON.stringify({
+                memory: (json as { memory: Int8Array })["memory"],
+              })
+            );
+            break;
+          case "/setPC":
+            await this.state.storage.put("pc", (json as { pc: number })["pc"]);
+            this.broadcast(
+              JSON.stringify({
+                pc: (json as { pc: number })["pc"],
+              })
+            );
+            break;
+          case "/setIndex":
+            await this.state.storage.put(
+              "index",
+              (json as { index: number })["index"]
+            );
+            this.broadcast(
+              JSON.stringify({
+                index: (json as { index: number })["index"],
+              })
+            );
+            break;
+          case "/resetRegs":
+            await this.state.storage.put(
+              "regs",
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            );
+            this.broadcast(
+              JSON.stringify({
+                regs: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              })
+            );
+            break;
+          case "/setReg":
+            let newRegs: Int8Array =
+              (await this.state.storage.get("regs")) || new Int8Array(16);
+            newRegs[(json as { regIndex: number; value: number }).regIndex] = (
+              json as { regIndex: number; value: number }
+            ).value;
+            await this.state.storage.put("regs", newRegs);
+            this.broadcast(JSON.stringify({ regs: newRegs }));
+            break;
+          case "/setStack":
+            await this.state.storage.put(
+              "stack",
+              (json as { stack: number[] })["stack"]
+            );
+            this.broadcast(
+              JSON.stringify({ stack: (json as { stack: number[] })["stack"] })
+            );
+            break;
+          case "/setDelayTimer":
+            await this.state.storage.put(
+              "delayTimer",
+              (json as { delayTimer: number })["delayTimer"]
+            );
+            this.broadcast(
+              JSON.stringify({
+                delayTimer: (json as { delayTimer: number })["delayTimer"],
+              })
+            );
+            break;
+          case "/setSoundTimer":
+            await this.state.storage.put(
+              "soundTimer",
+              (json as { soundTimer: number })["soundTimer"]
+            );
+            this.broadcast(
+              JSON.stringify({
+                soundTimer: (json as { soundTimer: number })["soundTimer"],
+              })
+            );
+            break;
+          case "/setShouldQuit":
+            await this.state.storage.put(
+              "shouldQuit",
+              (json as { shouldQuit: boolean })["shouldQuit"]
+            );
+            this.broadcast(
+              JSON.stringify({
+                shouldQuit: (json as { shouldQuit: boolean })["shouldQuit"],
+              })
+            );
+            break;
+          case "/getMemory":
+            websocket.send(JSON.stringify({ memory }));
+          case "/getPC":
+            websocket.send(JSON.stringify({ pc }));
+          case "/getIndex":
+            websocket.send(JSON.stringify({ index }));
+          case "/getRegs":
+            websocket.send(JSON.stringify({ regs }));
+          case "/getStack":
+            websocket.send(JSON.stringify({ stack }));
+          case "/getDelayTimer":
+            websocket.send(JSON.stringify({ delayTimer }));
+          case "/getSoundTimer":
+            websocket.send(JSON.stringify({ soundTimer }));
+          case "/getShouldQuit":
+            websocket.send(JSON.stringify({ shouldQuit }));
+        }
+      } catch (err: any) {
+        websocket.send(JSON.stringify({ error: err.message }));
       }
-    } else {
-      switch (url.pathname) {
-        case "/getMemory":
-          return new Response(JSON.stringify({ memory }));
-        case "/getPC":
-          return new Response(JSON.stringify({ pc }));
-        case "/getIndex":
-          return new Response(JSON.stringify({ index }));
-        case "/getRegs":
-          return new Response(JSON.stringify({ regs }));
-        case "/getStack":
-          return new Response(JSON.stringify({ stack }));
-        case "/getDelayTimer":
-          return new Response(JSON.stringify({ delayTimer }));
-        case "/getSoundTimer":
-          return new Response(JSON.stringify({ soundTimer }));
-        case "/getShouldQuit":
-          return new Response(JSON.stringify({ shouldQuit }));
-        default:
-          return new Response("Unknown path", { status: 404 });
+    });
+  }
+
+  broadcast(message: string) {
+    this.sessions.forEach((session) => {
+      try {
+        session.send(message);
+        return true;
+      } catch (err) {
+        return;
       }
-    }
-    return new Response("Unknown path", { status: 404 });
+    });
   }
 }
